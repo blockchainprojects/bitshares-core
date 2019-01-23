25642f255b2c6d87f21ced6430e2e12ce8746501
dimfred <dimfred.1337@web.de>
2019-01-23 10:57:57 +0100

    api added login_signed

--- libraries/app/include/graphene/app/api.hpp
+++ libraries/app/include/graphene/app/api.hpp
@@ -523,6 +523,8 @@ namespace graphene { namespace app {
           * has sucessfully authenticated.
           */
          bool login(const string& user, const string& password);
+         bool login_signed(const string& b64_encoded_trx);
+
          /// @brief Retrieve the network block API
          fc::api<block_api> block()const;
          /// @brief Retrieve the network broadcast API
@@ -556,6 +558,9 @@ namespace graphene { namespace app {
          optional< fc::api<asset_api> > _asset_api;
          optional< fc::api<orders_api> > _orders_api;
          optional< fc::api<graphene::debug_witness::debug_api> > _debug_api;
+
+         bool verify_api_access_info_signed( const account_object& acc, 
+            const api_access_info_signed& api_access_info );
    };
 
 }}  // graphene::app
@@ -631,4 +636,5 @@ FC_API(graphene::app::login_api,
        (asset)
        (orders)
        (debug)
+       (login_signed)
      )
