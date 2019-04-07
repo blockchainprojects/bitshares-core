ed66982aa2fc1a4a6a80bbc92b10f1d8c532c3f1
dimfred <dimfred.1337@web.de>
2019-04-08 01:32:30 +0200

    client

--- libraries/plugins/zeromq/client_sample.py
+++ libraries/plugins/zeromq/client_sample.py
@@ -1,14 +1,30 @@
+#!/usr/bin/env python2.7
 import zmq
 import sys
 import time
 
+endpoint = "tcp://127.0.0.1:"
 port = "5556"
+sub = ""
+
+try:
+    sub = sys.argv[1]
+except:
+    pass
+
+psub = sub
+if sub == "":
+    psub = "all"
+
+print "Connecting to server %s:%s subscribing for: %s" % (endpoint, port, psub)
 
 context = zmq.Context()
-print "Connecting to server..."
-socket = context.socket(zmq.PULL)
-socket.connect ("tcp://127.0.0.1:%s" % port)
-
+socket = context.socket(zmq.SUB)
+socket.setsockopt(zmq.SUBSCRIBE, b"%s" % sub )
+endpoint = endpoint + port
+socket.connect(endpoint)
+print "\n"
 while True:
     message = socket.recv()
     print message
+    print "\n"
