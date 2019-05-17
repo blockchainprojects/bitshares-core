03687c77f110ec15af89918355fb3f64fc3dd577
dimfred <dimfred.1337@web.de>
2019-05-17 11:09:29 +0200

    new buckets

--- libraries/plugins/net_stats/net_stats.cpp
+++ libraries/plugins/net_stats/net_stats.cpp
@@ -36,131 +36,134 @@ namespace graphene { namespace net_stats {
 
 namespace detail {
 
-class net_stats_impl {
+class net_stats_impl 
+{
 public:
-    net_stats_impl(net_stats_plugin& _plugin)
-        : _self(_plugin) 
-        {
-            registry = std::make_shared<prometheus::Registry>();
+	net_stats_impl(net_stats_plugin& _plugin)
+		: _self(_plugin) 
+	{
+      registry = std::make_shared<prometheus::Registry>();
 
+      auto& msg_cnt_fam = prometheus::BuildCounter()
+         .Name( "p2p_message_cnt" )
+         .Help( "How much messages has this node received/transmitted?" )
+         .Labels( {} )
+         .Register( *registry );
 
-            auto& msg_cnt_fam = prometheus::BuildCounter()
-                                    .Name( "p2p_message_cnt" )
-                                    .Help( "How much messages has this node received/transmitted?" )
-                                    .Labels( {} )
-                                    .Register( *registry );
-
-            msg_rx_cnt = &msg_cnt_fam.Add( { {"type", "rx"} } );
-            msg_tx_cnt = &msg_cnt_fam.Add( { {"type", "tx"} } );
-
-
-
-            auto& msg_size_hsg_fam = prometheus::BuildHistogram()
-                                        .Name( "p2p_message_size_hsg" )
-                                        .Help( "How much messages with which size were received/transmitted?" )
-                                        .Labels( {} )
-                                        .Register( *registry );
-            
-            prometheus::Histogram::BucketBoundaries msg_size_boundaries;
-            for( uint16_t bucket = 256; bucket <= 4096; bucket += 256 )
-                msg_size_boundaries.push_back( bucket );
-
-            msg_rx_size_hsg = &msg_size_hsg_fam.Add( { {"type", "rx"} }, msg_size_boundaries );
-            msg_tx_size_hsg = &msg_size_hsg_fam.Add( { {"type", "tx"} }, msg_size_boundaries );
+      msg_rx_cnt = &msg_cnt_fam.Add( { {"type", "rx"} } );
+      msg_tx_cnt = &msg_cnt_fam.Add( { {"type", "tx"} } );
 
 
 
-            auto& block_latency_hsg_fam = prometheus::BuildHistogram()
-                                                        .Name( "p2p_block_latency_hsg" )
-                                                        .Help( "How much block messages with which latency were received?" )
-                                                        .Labels( {} )
-                                                        .Register( *registry );
-            
-            prometheus::Histogram::BucketBoundaries block_latency_boundaries;
-            for( double bucket = 0.0; bucket < 10000; bucket += 500 )
-                block_latency_boundaries.push_back( bucket );
+      auto& msg_size_hsg_fam = prometheus::BuildHistogram()
+         .Name( "p2p_message_size_hsg" )
+         .Help( "How much messages with which size were received/transmitted?" )
+         .Labels( {} )
+         .Register( *registry );
+      
+      prometheus::Histogram::BucketBoundaries msg_size_boundaries;
+      for( uint16_t bucket = 256; bucket <= 4096; bucket += 256 )
+            msg_size_boundaries.push_back( bucket );
 
-            block_latency_hsg = &block_latency_hsg_fam.Add( {}, block_latency_boundaries );            
-        }
-    ~net_stats_impl() = default;
+      msg_rx_size_hsg = &msg_size_hsg_fam.Add( { {"type", "rx"} }, msg_size_boundaries );
+      msg_tx_size_hsg = &msg_size_hsg_fam.Add( { {"type", "tx"} }, msg_size_boundaries );
 
-    net_stats_plugin& _self;
 
-    void process_event(const net::network_statistics_event& event) {
-        //ilog("Network statistic event: type=${type}, size=${size}, peer=${peer}", ("type", event.event_type)("size", event.event_data.size())("peer", event.remote_endpoint));
-        switch( event.event_type )
-        {
-            case net::network_statistics_event::EventType::MessageReceived:
-            {
-                msg_rx_cnt->Increment();  
 
-                msg_rx_size_hsg->Observe( event.event_data.size() );
+      auto& block_latency_hsg_fam = prometheus::BuildHistogram()
+         .Name( "p2p_block_latency_hsg" )
+         .Help( "How much block messages with which latency were received?" )
+         .Labels( {} )
+         .Register( *registry );
+      
+      prometheus::Histogram::BucketBoundaries block_latency_boundaries;
+      for( double bucket = 0.0; bucket < 5*60*1000; bucket += 500 ) 
+            block_latency_boundaries.push_back( bucket );
 
-                break;
-            }
-            case net::network_statistics_event::EventType::MessageSent:
-            {
-                msg_tx_cnt->Increment();
+      block_latency_hsg = &block_latency_hsg_fam.Add( {}, block_latency_boundaries );            
+   }
+   ~net_stats_impl() = default;
 
-                msg_tx_size_hsg->Observe( event.event_data.size() );
+   net_stats_plugin& _self;
 
-                break;
-            }
-            default:
-                break;
+   void process_event(const net::network_statistics_event& event) 
+   {
+      //ilog("Network statistic event: type=${type}, size=${size}, peer=${peer}", ("type", event.event_type)("size", event.event_data.size())("peer", event.remote_endpoint));
+      switch( event.event_type )
+      {
+         case net::network_statistics_event::EventType::MessageReceived:
+         {
+            msg_rx_cnt->Increment();  
 
-        }
-    }
+            msg_rx_size_hsg->Observe( event.event_data.size() );
 
-    void on_block_handled( int64_t block_latency ) 
-    {
-        // to not publish block_latency while in sync mode
-        static const int64_t MAX_LATENCY = 5 * 60 * 1000;
-        
-        if( block_latency >= MAX_LATENCY )
-            block_latency_hsg->Observe( block_latency );
-    }
+            break;
+         }
+         case net::network_statistics_event::EventType::MessageSent:
+         {
+            msg_tx_cnt->Increment();
 
-    std::string exposer_endpoint = "127.0.0.1:8080";
-    // http server to expose prometheus metrics
-    std::shared_ptr<prometheus::Exposer> exposer;
-    // registry for all metric types
-    std::shared_ptr<prometheus::Registry> registry;
+            msg_tx_size_hsg->Observe( event.event_data.size() );
+
+            break;
+         }
+         default:
+               break;
+
+      }
+   }
+
+   void on_block_handled( int64_t block_latency ) 
+   {
+      // to not publish block_latency while in sync mode
+      static const int64_t MAX_LATENCY = 5 * 60 * 1000;
+      
+      if( block_latency >= MAX_LATENCY )
+         block_latency_hsg->Observe( block_latency );
+   }
+
+   std::string exposer_endpoint = "127.0.0.1:8080";
+   // http server to expose prometheus metrics
+   std::shared_ptr<prometheus::Exposer> exposer;
+   // registry for all metric types
+   std::shared_ptr<prometheus::Registry> registry;
+
 private:
-    // p2p received message counter
-    prometheus::Counter* msg_rx_cnt;
-    // p2p transmitted message counter
-    prometheus::Counter* msg_tx_cnt;
-    // p2p received message size histogram 
-    prometheus::Histogram* msg_rx_size_hsg;
-    // p2p transmitted message size histogram
-    prometheus::Histogram* msg_tx_size_hsg;
-    // p2p block latency
-    prometheus::Histogram* block_latency_hsg;
+   // p2p received message counter
+   prometheus::Counter* msg_rx_cnt;
+   // p2p transmitted message counter
+   prometheus::Counter* msg_tx_cnt;
+   // p2p received message size histogram 
+   prometheus::Histogram* msg_rx_size_hsg;
+   // p2p transmitted message size histogram
+   prometheus::Histogram* msg_tx_size_hsg;
+   // p2p block latency
+   prometheus::Histogram* block_latency_hsg;
 };
 } // end namespace detail
 
 net_stats_plugin::net_stats_plugin() :
-    my(new detail::net_stats_impl(*this)) {
+   my(new detail::net_stats_impl(*this)) 
+{
 }
 
 net_stats_plugin::~net_stats_plugin() = default;
 
 std::string net_stats_plugin::plugin_name()const {
-    return "net_stats";
+   return "net_stats";
 }
 
 std::string net_stats_plugin::plugin_description()const {
-    return "Plugin to receive and process statistics events from P2P network";
+   return "Plugin to receive and process statistics events from P2P network";
 }
 
 void net_stats_plugin::plugin_set_program_options(boost::program_options::options_description& cli,
-                                                  boost::program_options::options_description& cfg) {
-    cli.add_options()
-         ("net-stats-exposer-endpoint", boost::program_options::value<std::string>(),
-                "net stats metric exposer endpoint (default 127.0.0.1:8080)");
+                                             boost::program_options::options_description& cfg) {
+   cli.add_options()
+      ("net-stats-exposer-endpoint", boost::program_options::value<std::string>(),
+               "net stats metric exposer endpoint (default 127.0.0.1:8080)");
 
-    cfg.add(cli);
+   cfg.add(cli);
 }
 
 void net_stats_plugin::plugin_initialize(const boost::program_options::variables_map& options)
@@ -173,18 +176,18 @@ void net_stats_plugin::plugin_initialize(const boost::program_options::variables
 }
 
 void net_stats_plugin::plugin_startup() {
-    ilog("net_stats: plugin_startup() begin");
-    FC_ASSERT(&p2p_node() != nullptr, "P2P node not yet set! Unable to initialize");
-    p2p_node().subscribe_network_stats([this](const net::network_statistics_event& event) {
-        // Copy event and schedule processing for later; return immediately to unblock network
-        fc::async([event, data=event.event_data, this]() {
-            net::network_statistics_event copy(event.event_type, event.remote_endpoint, data, event.event_time);
-            my->process_event(copy);
-        });
-    });
-    // register block_handled signal to emit the block_latency
-    // TODO save return for disconnect
-    app().block_handled.connect( boost::bind( &detail::net_stats_impl::on_block_handled, &*my, _1 ) );
+   ilog("net_stats: plugin_startup() begin");
+   FC_ASSERT(&p2p_node() != nullptr, "P2P node not yet set! Unable to initialize");
+   p2p_node().subscribe_network_stats([this](const net::network_statistics_event& event) {
+      // Copy event and schedule processing for later; return immediately to unblock network
+      fc::async([event, data=event.event_data, this]() {
+         net::network_statistics_event copy(event.event_type, event.remote_endpoint, data, event.event_time);
+         my->process_event(copy);
+      });
+   });
+   // register block_handled signal to emit the block_latency
+   // TODO save return for disconnect
+   app().block_handled.connect( boost::bind( &detail::net_stats_impl::on_block_handled, &*my, _1 ) );
 }
 
 } }
